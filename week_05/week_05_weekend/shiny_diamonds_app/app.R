9e137ac52cc437c4c133b9a9217ed01687c8764a
Lesley Duff <137374442+dataquine@users.noreply.github.com>
2023-08-27 19:15:01 +0100

    Add histogram, Start carat slider

--- week_05/week_05_weekend/shiny_diamonds_app/app.R
+++ week_05/week_05_weekend/shiny_diamonds_app/app.R
@@ -7,62 +7,73 @@
 #    http://shiny.rstudio.com/
 #
 
-library(bslib)
-library(shiny)
+#library(bslib)
+#library(shiny)
+library(tidyverse)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   theme = bs_theme(bootswatch = "minty"),
-  
-    # Application title
-    titlePanel("Diamonds Data"),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-          checkboxGroupInput( # ?checkboxGroupInput
-            "cut", 
-            strong("Filter by cut"),
-            choices = unique(diamonds$cut), 
-            selected = unique(diamonds$cut)
-          )
-           # sliderInput("bins",
-            #            "Number of bins:",
-             #           min = 1,
-              #          max = 50,
-               #         value = 30)
-        ),
+  # Application title
+  titlePanel("Diamonds Data"),
 
-        # Show a scatter plot
-        mainPanel(
-           plotOutput("scatterPlot")
-        )
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      checkboxGroupInput( # ?checkboxGroupInput
+        "cut_input",
+        strong("Filter by cut"),
+        choices = unique(diamonds$cut),
+        selected = unique(diamonds$cut),
+       # inline = TRUE,
+      ),
+       sliderInput("carat_input",
+                  strong("Carat:"),
+                 min = min(diamonds$carat),
+                max = max(diamonds$carat),
+               value = range(diamonds$carat)
+               )
+    ),
+
+    # Show a scatter plot
+    mainPanel(
+      plotOutput("histogram_plot")
     )
+  )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-  filteredDiamonds <- reactive({
-    req(input$cut) # ?req
+server <- function(input, output, session) {
+  filtered_data <- reactive({
+    #req(input$cut_input) # ?req
     diamonds %>%
-      filter(cut %in% input$cut)
-    })
-  
-    output$histogramPlot <- renderPlot({
-      filteredDiamonds() %>% 
-        ggplot(aes(x = price))+
-        geom_histogram(color = "white")
-      
-        # generate bins based on input$bins from ui.R
-#        x    <- faithful[, 2]
- #       bins <- seq(min(x), max(x), length.out = input$bins + 1)
+      #select(price, cut)
+      filter(cut %in% input$cut_input)
+  })
+  #print(filtered_data)
 
-        # draw the histogram with the specified number of bins
-  #      hist(x, breaks = bins, col = 'darkgray', border = 'white',
-   #          xlab = 'Waiting time to next eruption (in mins)',
-    #         main = 'Histogram of waiting times')
-    })
+  output$histogram_plot <- renderPlot({
+    #print(filtered_data())
+    ggplot(filtered_data(), aes(x = price)) +
+      geom_histogram(color = "white", fill = "steelblue")
+
+    # generate bins based on input$bins from ui.R
+ #   x <- faithful[, 2]
+  #  bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+    # draw the histogram with the specified number of bins
+   # hist(x,
+    #  breaks = bins, col = "darkgray", border = "white",
+     # xlab = "Waiting time to next eruption (in mins)",
+      #main = "Histogram of waiting times"
+    #)
+  })
+
+  output$table_output <- DT::renderDataTable({
+    filtered_data()
+  })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
