c159d59f2b0864f0a58cff15e35956dbeface5a7
Lesley Duff <137374442+dataquine@users.noreply.github.com>
2023-08-27 18:49:51 +0100

    Add filter by cut

--- week_05/week_05_weekend/shiny_diamonds_app/app.R
+++ week_05/week_05_weekend/shiny_diamonds_app/app.R
@@ -7,43 +7,60 @@
 #    http://shiny.rstudio.com/
 #
 
+library(bslib)
 library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
+  theme = bs_theme(bootswatch = "minty"),
+  
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Diamonds Data"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+          checkboxGroupInput( # ?checkboxGroupInput
+            "cut", 
+            strong("Filter by cut"),
+            choices = unique(diamonds$cut), 
+            selected = unique(diamonds$cut)
+          )
+           # sliderInput("bins",
+            #            "Number of bins:",
+             #           min = 1,
+              #          max = 50,
+               #         value = 30)
         ),
 
-        # Show a plot of the generated distribution
+        # Show a scatter plot
         mainPanel(
-           plotOutput("distPlot")
+           plotOutput("scatterPlot")
         )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
+  filteredDiamonds <- reactive({
+    req(input$cut) # ?req
+    diamonds %>%
+      filter(cut %in% input$cut)
+    })
+  
+    output$histogramPlot <- renderPlot({
+      filteredDiamonds() %>% 
+        ggplot(aes(x = price))+
+        geom_histogram(color = "white")
+      
         # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+#        x    <- faithful[, 2]
+ #       bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
         # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+  #      hist(x, breaks = bins, col = 'darkgray', border = 'white',
+   #          xlab = 'Waiting time to next eruption (in mins)',
+    #         main = 'Histogram of waiting times')
     })
 }
 
